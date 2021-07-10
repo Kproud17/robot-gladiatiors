77133fbf2247d7d4bb8e5e83af1e44946535e656
Kproud17 <kristenmproud@gmail.com>
2021-07-10 19:45:46 -0400

    update the shop () function to handle integer inputs

--- assets/js/game.js
+++ assets/js/game.js
@@ -97,9 +97,10 @@ var fightOrSkip = function(){
 
       // return true if player wants to leave
       return true;
-      return false;
-      shop();
+      
     }
+      shop();
+    
   }
 }
 
@@ -234,45 +235,26 @@ if (playAgainConfirm) {
 var shop = function() {
   //ask player what they want to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
 
   );
+
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+
 // use switch to carry out action 
 switch (shopOptionPrompt) {
-  case "REFILL": // new case
-  case "refill":
-    if (playerInfo.refillHealth >= 7) {
-      window.alert("Refilling player's health by 20 for 7 dollars.");
-
-      playerInfo.health = playerInfo.health + 20;
-      playerInfo.money = playerInfo.money - 7;
-    }
-    else {
-      window.alert("You don't have enough money!");
-    }
-
+  case 1:
+    playerInfo.refillHealth();
     break;
-  case "UPGRADE": // new case
-  case "upgrade":
-    if (playerInfo.upgradeAttack >= 7) {
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-      playerInfo.attack = playerInfo.attack + 6;
-      playerInfo.money = playerInfo.money - 7;
-    }
-    else {
-      window.alert("You don't have enough money!");
-    }
-
+  case 2: playerInfo.upgradeAttack();
     break;
-  case "LEAVE": // new case
-  case "leave":
+  case 3: 
     window.alert("Leaving the store.");
     break;
   default:
     window.alert("You did not pick a valid option. Try again.");
     shop();
-    break;
+    break;      
 }
 };
 //Do you want to play again
